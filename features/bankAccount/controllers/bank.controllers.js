76e668ebd9bac888cb5cd55904bca95bcacc82f0
Mridupawan Bordoloi <mridupawan503@gmail.com>
2025-08-16 23:53:25 +0530

    Fix import statement for asyncHandler in bank controllers

--- features/bankAccount/controllers/bank.controllers.js
+++ features/bankAccount/controllers/bank.controllers.js
@@ -1,4 +1,4 @@
-import asyncHandler from "../../../utils/asyncHandler.js";
+import { asyncHandler } from "@/../../utils/asyncHandler.js";
 import Bank from "../models/bank.model.js";
 import { ApiResponse } from "../../../utils/ApiResponse.js";
 
