6714107421ca815fc7f0c76a166e813aa8374d55
Mridupawan Bordoloi <mridupawan503@gmail.com>
2025-08-17 21:58:50 +0530

    update

--- features/index.js
+++ features/index.js
@@ -1,7 +1,16 @@
 import express from "express";
 import authRouter from "./auth/routes/user.routes.js";
+import transactionRouter from "./transaction/routes/transaction.routes.js";
+import { authenticateToken } from "../middlewares/auth.middleware.js";
+import taskRouter from './task/routes/task.routes.js'
+import bankRouter from './bankAccount/routes/bank.routes.js'
+
 const router = express.Router();
 
 router.use("/auth", authRouter);
+router.use("/transaction", authenticateToken, transactionRouter);
+router.use("/category", transactionRouter);
+router.use("/bank-account", bankRouter);
+router.use("/task", taskRouter);
 
 export default router;
